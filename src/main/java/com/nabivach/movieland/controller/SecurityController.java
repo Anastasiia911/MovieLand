88a5bda2f8a6608e207d034de6670c774b1586c4
Anastasiia911 <nabina91@gmail.com>
2016-06-30 11:33:23 +0300

    S1

--- src/main/java/com/nabivach/movieland/controller/SecurityController.java
+++ src/main/java/com/nabivach/movieland/controller/SecurityController.java
@@ -1,10 +1,16 @@
 package com.nabivach.movieland.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+@RequestMapping(name = "/v1")
 @Controller
 public class SecurityController {
 
-  
+   // @RequestMapping(value="/login",method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+// public String
+
+
+
 }
