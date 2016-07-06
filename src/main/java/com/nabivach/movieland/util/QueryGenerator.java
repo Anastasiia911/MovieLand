4506dd1a2e5424e33b1c6bf2c173ba9f412d8900
Anastasiia Nabivach <anastasiia.nabivach@db.com>
2016-07-06 19:06:51 +0300

    T-2

--- src/main/java/com/nabivach/movieland/util/QueryGenerator.java
+++ src/main/java/com/nabivach/movieland/util/QueryGenerator.java
@@ -21,14 +21,14 @@ public class QueryGenerator {
     private final String WHERE_CLAUSE = " where 1=1 ";
     private int countMoviesPerPage =5;
 
-    public StringBuilder getGeneratedQueryAllMovies(MovieRequest movieRequest) {
+    public String getGeneratedQueryAllMovies(MovieRequest movieRequest) {
         StringBuilder sql = new StringBuilder(getAllMoviesSQL + " order by rating " + movieRequest.getRating() + ", price " + movieRequest.getPrice());
         sql.append(" LIMIT ");
         sql.append(countMoviesPerPage);
         sql.append(" OFFSET ");
         sql.append(countMoviesPerPage * (movieRequest.getPageNumber() - 1));
 
-        return sql;
+        return sql.toString();
     }
 
     public String getMoviesSearchRequestSQL(MovieSearchRequest movieSearchRequest) {
